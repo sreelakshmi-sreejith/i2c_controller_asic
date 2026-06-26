241e28a46e371119c923ed59c3950810d01bfc83
Sreelakshmi S S <sreelakshmi.xa@gmail.com>
2026-06-26 21:52:50 +0530

    Create clk_gen.v

---
+++ RTL/clk_gen.v
@@ -0,0 +1,20 @@
+module clkgen_200kHz(
+    input clk_100MHz,
+    output clk_200kHz
+);
+
+reg [7:0] counter = 8'h00;
+reg clk_reg = 1'b1;
+
+always @(posedge clk_100MHz) begin
+    if(counter == 249) begin
+        counter <= 8'h00;
+        clk_reg <= ~clk_reg;
+    end
+    else
+        counter <= counter + 1;
+end
+
+assign clk_200kHz = clk_reg;
+
+endmodule
